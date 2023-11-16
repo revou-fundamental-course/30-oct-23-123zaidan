29f745782e30228523a78fc6ebccfe3f46262558
123zaidan <127397861+123zaidan@users.noreply.github.com>
2023-11-16 15:10:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -73,3 +73,52 @@ document.getElementById("calculate").addEventListener("click", function () {
     a.click();
     window.URL.revokeObjectURL(url);
   });
+  function openWhatsApp() {
+    // Gantilah nomor WhatsApp dan pesan sesuai kebutuhan Anda
+    const phoneNumber = "085784888824";
+    const message = "Halo, saya tertarik untuk konsultasi. Apakah Anda tersedia?";
+  
+    // Buat URL untuk membuka WhatsApp dengan nomor dan pesan tertentu
+    const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+  
+    // Buka jendela baru atau tab dengan URL WhatsApp
+    window.open(whatsappURL, "_blank");
+  }
+  // script.js
+
+document.getElementById("kirim").addEventListener("click", function () {
+  const nim = document.getElementById("nim").value;
+  const alamat = document.getElementById("alamat").value;
+  const domisili = document.getElementById("domisili").value;
+  const tglLahir = document.getElementById("tglLahir").value;
+  const jurusan = document.getElementById("jurusan").value;
+
+  const message = `NIM: ${nim}%0AAlamat: ${alamat}%0ADomisili: ${domisili}%0ATanggal Lahir: ${tglLahir}%0AJurusan: ${jurusan}`;
+
+  const waURL = `https://api.whatsapp.com/send?phone=6285784888824&text=${encodeURIComponent(message)}`;
+
+  window.open(waURL, "_blank");
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const images = [
+    "http://www.umm.ac.id/files/image/DJI_0135-copy.jpg",
+    "https://akcdn.detik.net.id/community/media/visual/2022/03/11/universitas-muhammadiyah-malang_169.jpeg?w=700&q=90",
+    // Add more image URLs as needed
+  ];
+
+  const backgroundImage = document.getElementById("background-image");
+
+  let currentIndex = 0;
+
+  function changeBackground() {
+    currentIndex = (currentIndex + 1) % images.length;
+    backgroundImage.style.opacity = 0;
+    setTimeout(() => {
+      backgroundImage.style.backgroundImage = `url('${images[currentIndex]}')`;
+      backgroundImage.style.opacity = 1;
+    }, 1000);
+  }
+
+  setInterval(changeBackground, 5000); // Change every 5 seconds
+});
